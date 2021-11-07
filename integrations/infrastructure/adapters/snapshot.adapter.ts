45aaec3d892c8cb3dad182dd8821d94ddce94400
reiwa <reiwa@outlook.com.vn>
2021-11-07 23:56:23 +0900

    fix

--- integrations/infrastructure/adapters/snapshot.adapter.ts
+++ integrations/infrastructure/adapters/snapshot.adapter.ts
@@ -1,7 +1,8 @@
 import { captureException } from "@sentry/node"
+import chrome from "chrome-aws-lambda"
 import { Id, Url } from "integrations/domain"
 import { tmpdir } from "os"
-import puppeteer from "puppeteer"
+import puppeteer from "puppeteer-core"
 import { injectable } from "tsyringe"
 
 @injectable()
@@ -9,6 +10,9 @@ export class SnapshotAdapter {
   async capture(props: { fileId: Id; url: Url }) {
     try {
       const browser = await puppeteer.launch({
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
         defaultViewport: {
           width: 1024,
           height: 1024,
