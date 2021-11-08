56ad1b82fb957882ab1c5f760f3af82e764980f8
reiwa <reiwa@outlook.com.vn>
2021-11-08 22:35:48 +0900

    update

--- integrations/infrastructure/adapters/snapshot.adapter.ts
+++ integrations/infrastructure/adapters/snapshot.adapter.ts
@@ -7,6 +7,12 @@ import { injectable } from "tsyringe"
 @injectable()
 export class SnapshotAdapter {
   async capture(props: { fileId: Id; url: Url }) {
+    const puppeteer = require("puppeteer-core")
+    const chrome = require("chrome-aws-lambda")
+
+    console.log(puppeteer)
+    console.log(chrome)
+
     try {
       // const browser = await puppeteer.launch({
       //   args: chrome.args,
